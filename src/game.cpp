a7697cc03104a13171f0b4392910b165ccf4a81b
Tyler Suehr <tylersuehr@gmail.com>
2024-12-10 09:00:03 -0500

    Draws the grid

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,6 @@
 #include "game.hpp"
 
-Game::Game(const Vector2 &size): m_size(size) {
+Game::Game(const Vector2 &size): m_size(size), m_grid(Grid()) {
 }
 
 void Game::on_process_input() {
@@ -13,4 +13,5 @@ void Game::on_update() {
 
 void Game::on_render() {
     ClearBackground(BLACK);
+    m_grid.draw();
 }
--- src/game.hpp
+++ src/game.hpp
@@ -2,10 +2,12 @@
 #define TETRIS_GAME_HPP
 
 #include <raylib.h>
+#include "grid.hpp"
 
 class Game {
 private:
     const Vector2 m_size;
+    Grid m_grid;
 public:
     Game(const Vector2 &size);
     void on_process_input();
